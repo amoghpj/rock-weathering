850f14d6af32008c9906516866066604205b5db6
amoghjalihal <amogh_jalihal@hms.harvard.edu>
2025-06-18 10:51:16 -0400

    Updates

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='ajutils',
+      version='0.1',
+      description="Model and code for rock weathering paper",
+      author='Amogh Jalihal',
+      author_email='amogh.jalihal@gmail.com',
+      license='MIT',
+      packages=['rock_weathering'],
+      zip_safe=False)
